d3c6bcecdbff2c6cf8ee1fc5f35b83349086e973
Doomsday1102 <die2002alexsander@gmail.com>
2024-07-23 00:02:49 -0500

    avance final del dia

--- projects/01-tic-tac-toe/src/App.jsx
+++ projects/01-tic-tac-toe/src/App.jsx
@@ -5,13 +5,20 @@ import { Square } from "./components/Square"
 import { TURNS } from "./constants"
 import { checkWinner,checkEndGame } from "./logic/board"
 import { WinnerModal } from "./components/WinnerModal"
+import { resetGameStorage, saveGameToStorage } from "./logic/storage/storage"
 function App() {
 
-  const [board, setBoard] = useState(
-    Array(9).fill(null)
-  )
-  const [turn,setTurn] = useState(TURNS.X)
-  //null no hay ganaor, false empate
+  const [board, setBoard] = useState(()=>{
+    const boardFromStorage= window.localStorage.getItem('board')
+     if(boardFromStorage) return JSON.parse(boardFromStorage) 
+    return Array(9).fill(null)
+  })
+
+  const [turn,setTurn] = useState(()=>{
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X
+  })
+
   const [winner,setWinner] = useState(null)
 
   
@@ -20,9 +27,8 @@ function App() {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
+    resetGameStorage()
   }
-
- 
 
   const updateBoard= (index) => {
 
@@ -37,6 +43,11 @@ function App() {
     //cambiar el turno
     const newTurn = turn=== TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+    //guardar partida
+    saveGameToStorage({
+      board: newBoard,
+      turn: newTurn
+    })
     //revisar ganador
     const newWinner=checkWinner(newBoard)
     if(newWinner){
